d6959b00471f7eae632a3b02e82614f432f9a7e0
Bruno Fonseca Pietra <80489061+brunopietra@users.noreply.github.com>
2022-05-22 23:41:19 -0300

    Update time.java

--- time.java
+++ time.java
@@ -9,7 +9,7 @@ public class time {
     private int idClube;
     private String nome;
     private String cnpj;
-    private String estado;
+    private String cidade;
     private int pJogadas;
     private int pontos;
 
@@ -17,31 +17,43 @@ public class time {
         this.idClube = -1;
         this.nome = "";
         this.cnpj = "";
-        this.estado = "";
+        this.cidade = "";
         this.pJogadas = -1;
         this.pontos = -1;
     }
 
-    public time(String nome, String cnpj, String estado) throws IOException{
+    public time(String nome, String cnpj, String cidade) throws IOException{
         this.idClube = setId();
         this.nome = nome;
         this.cnpj = cnpj;
-        this.estado = estado;
+        this.cidade = cidade;
         this.pJogadas = 0;
         this.pontos = 0;
     }
 
-    public time(int id, String nome, String cnpj, String estado, int pJogadas, int pontos) throws IOException{
+    public time(int id, String nome, String cnpj, String cidade, int pJogadas, int pontos) throws IOException{
         this.idClube = id;
         this.nome = nome;
         this.cnpj = cnpj;
-        this.estado = estado;
+        this.cidade = cidade;
         this.pJogadas = pJogadas;
         this.pontos = pontos;
     }
 
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCnpj(){
+        return cnpj;
+    }
+
+    public String getCidade(){
+        return cidade;
+    }
+
     //setId serve para descobrir o Id do time. A funcao acessa o cabecalho do arquivo .db, alem de o modificar
-    public int setId() throws IOException{
+    private int setId() throws IOException{
         RandomAccessFile arq = new RandomAccessFile("dados/times.db", "rw");
         arq.seek(0);
         int id;
@@ -51,7 +63,7 @@ public class time {
             arq.read(ba);
             ByteArrayInputStream bais = new ByteArrayInputStream(ba);
             DataInputStream dis = new DataInputStream(bais);
-            id = dis.readInt();
+            id = dis.readInt() + 1;
 
         
 
@@ -59,7 +71,7 @@ public class time {
         byte[] ba2;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
-        dos.writeInt(id + 1);
+        dos.writeInt(id);
         ba2 = baos.toByteArray();
         arq.seek(0);
         arq.write(ba2);
@@ -68,18 +80,33 @@ public class time {
 
         return id;
     }
+
+    public int getId() {
+        return this.idClube;
+    }
+
+
+
     public void setpJogadas(int num){
         this.pJogadas += num;
+    }
+
+    public int getpJogadas(){
+        return pJogadas;
     }
 
     public void setPontos(int num){
         this.pontos += num;
     }
 
+    public int getPontos(){
+        return pontos;
+    }
+
 
     public void print() {
 
-        System.out.printf("id: %d\n nome: %s\n cnpj: %s\n estado: %s\n partidas jogadas: %d\n pontos: %d\n", this.idClube, this.nome, this.cnpj, this.estado, this.pJogadas, this.pontos);
+        System.out.printf("id: %d\n nome: %s\n cnpj: %s\n cidade: %s\n partidas jogadas: %d\n pontos: %d\n", this.idClube, this.nome, this.cnpj, this.cidade, this.pJogadas, this.pontos);
         
         /*"\nID....: " + this.idClube + "\nNome.: " + this.nome + "\nCNPJ.: " + this.cnpj + "\nPreço.: R$ "
                 + df.format(this.preco);*/
@@ -93,7 +120,7 @@ public class time {
         dos.writeInt(idClube);
         dos.writeUTF(nome);
         dos.writeUTF(cnpj);
-        dos.writeUTF(estado);
+        dos.writeUTF(cidade);
         dos.writeInt(pJogadas);
         dos.writeInt(pontos);
         return baos.toByteArray();
@@ -108,7 +135,7 @@ public class time {
 
         cnpj = dis.readUTF();
 
-        estado = dis.readUTF();
+        cidade = dis.readUTF();
 
         pJogadas = dis.readInt();
 
